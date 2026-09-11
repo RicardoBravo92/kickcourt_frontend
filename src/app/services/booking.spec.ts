fbf8496fa090ebf2e1b995d482c335177355e1fb
ricardobravo92 <ricardojbn5@gmail.com>
2026-09-11 18:21:22 -0400

    test: restore BookingService spec (Vitest runner) for coverage parity

---
+++ src/app/services/booking.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+
+import { BookingService } from './booking';
+
+describe('BookingService', () => {
+  let service: BookingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
+    service = TestBed.inject(BookingService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
